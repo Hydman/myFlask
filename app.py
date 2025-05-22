cbaba6c76739b1b693ad2659ecfc2a939123d132
Hydman <key9405618@gmail.com>
2025-05-22 20:01:31 +0800

    add 2 argument agonys&info

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    name = "Hydman"
+    age = 27
+    agonys = ["膽小", "傲慢", "懶惰"]
+    info = {
+        "job": "工程師",
+        "hobby": "打電動",
+        "favorite_food": "牛肉麵",
+        "favorite_color": "藍色"
+    }
+    return render_template('index.html', name=name, age=age, agonys=agonys, info=info)
 
 if __name__ == '__main__':
     app.run(debug=True)
