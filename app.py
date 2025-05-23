15072f41060bba8eb937094289ca4ac8af5eafde
Hydman <key9405618@gmail.com>
2025-05-23 19:26:16 +0800

    add page index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,13 @@ def home():
         "favorite_color": "藍色"
     }
     return render_template('index.html', name=name, age=age, agonys=agonys, info=info)
+
+@app.route('/index', methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+        nameInput = request.form.get('name')
+        return render_template("result.html", nameInput=nameInput)
+    return render_template("form.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
